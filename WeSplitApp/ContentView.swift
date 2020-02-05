e8ce8a0a178320483ab37b36f59f1d4fa00928ff
Rey <reycrittenden@yahoo.com>
2020-02-05 14:08:47 -0600

    Initial Commit

---
+++ WeSplitApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplitApp
+//
+//  Created by Crittenden, Rey  on 2/5/20.
+//  Copyright © 2020 Crittenden, Rey . All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
